bc8983d7fab17554433d6e1e43c1e5f9404fd2c7
Nellaiappan01 <nellaiappan264426@gmail.com>
2022-08-04 17:14:44 +0530

    react

---
+++ src/component/Mobile.jsx
@@ -0,0 +1,15 @@
+import React from'react'
+export function Mobiles(props){
+return(
+<>
+<div class="card">
+  <ul class="list-group list-group-flush"></ul>
+    <li class="list-group-item">{props.e}</li>
+    <li class="list-group-item">{props.n}</li>
+    
+</div>
+</>
+);
+}
+
+
